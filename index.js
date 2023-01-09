f6467457b5eff00d54cc62cce5c4ee509cf45356
JOSE ALARCON MOLINA <jos3.4larc0n@gmail.com>
2023-01-08 23:27:10 -0500

    Define newImage/newItem Functions, Create Items

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
+/*//create character//
 let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+document.body.append(greenCharacter)*/
+
+/*//create pine tree//
+let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)*/
+
+// Define function to create elements, less code(DRY)//
+
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/well.png', 500, 425)
+
+/*let sword = document.createElement('img')
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('click', function(){
+    sword.remove()
+})*/
+
+function newItem(url, left, bottom){
+    let item = newImage(url, left, bottom)
+    item.addEventListener('dblclick', ()=>{
+        object.remove()})
+}
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
