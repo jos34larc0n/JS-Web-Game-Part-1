dd6e08349512f4e43bcf40aac89c009e3e4c1829
JOSE ALARCON MOLINA <jos3.4larc0n@gmail.com>
2023-01-14 22:22:00 -0500

    create background function, remove item dblclick

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ document.body.append(pineTree)*/
 
 // Define function to create elements, less code(DRY)//
 
+function background(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
+
 function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
@@ -26,11 +35,18 @@ function newImage(url, left, bottom){
     return object
 }
 
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+background('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+background('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/tree.png', 200, 300)
-newImage('assets/crate.png', 150, 200)
 newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
 
 /*let sword = document.createElement('img')
@@ -46,10 +62,13 @@ sword.addEventListener('click', function(){
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
-    item.addEventListener('dblclick', ()=>{
-        object.remove()})
+    item.addEventListener("dblclick", function removeItem(){
+        item.remove()
+    })
 }
 
 newItem('assets/sword.png', 500, 405)
 newItem('assets/shield.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
+
+
